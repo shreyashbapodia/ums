24cf95c7226e7f363a5cd7eb110af69fb9c71619
shreyashbapodia <84457756+shreyashbapodia@users.noreply.github.com>
2022-11-07 20:56:34 +0530

    updated ui for statement.js

--- ums-ui/src/components/Statement/Statement.js
+++ ums-ui/src/components/Statement/Statement.js
@@ -1,5 +1,39 @@
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
 const Statement = () => {
-    return <h1>Statement</h1>;
+  return (<>
+    <Header/>
+    <div className="title">Statement</div>
+    <div className="form">
+        <form>
+          <div className="input-container">
+            <label>Customer Name </label>
+            <label>First_Name Last_Name </label>
+          </div>
+
+          <div className="input-container">
+            <label>Select the transaction </label>
+            <input type="text" name="type" required /> 
+          </div>
+
+          <div className="input-container">
+            <label>Transaction period from </label>
+            <input type="text" name="type" required /> 
+          </div>
+
+          <div className="input-container">
+            <label>Transaction period to </label>
+            <input type="text" name="type" required /> 
+          </div>
+
+          <div className="button-container">
+            <input type="submit" value="Submit"/>
+          </div>
+        </form>
+      </div>
+    <Footer/>
+    </>);
   };
   
   export default Statement;
